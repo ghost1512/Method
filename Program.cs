1bd504dd30bf7d4b3cdc0dc19f67464eef1aeeac
BaoHG <baohuynh291@gmail.com>
2024-04-11 19:24:56 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int[] myArray2 = { 10, 11, 12, 13, 14, 15, 16 };
+        Console.WriteLine($"Gia tri lon nhat: {maxvalue(myArray)}");
+        Console.WriteLine($"Gia tri lon nhat: {maxvalue(myArray2)}");
+    }
+    private static int maxvalue(int[] arrayValue)
+    {
+        int MaxValue = 0;
+        for (int i = 0; i < arrayValue.Length; i++)
+        {
+            if (arrayValue[i] > MaxValue)
+            {
+                MaxValue = arrayValue[i];
+            }
+        }
+        return MaxValue;
+    }
+}
